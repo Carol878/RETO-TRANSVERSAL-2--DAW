7ceaeb2370a9eb14c18aac2e9dbb36edd984cdf9
Nahir Carolina Torres <si.carolinatorres@gmail.com>
2026-04-17 21:50:49 +0200

    Permitir peticiones desde el front Angular

--- AplicacionBack/AplicacionBack/src/main/java/eventos/security/SecurityConfig.java
+++ AplicacionBack/AplicacionBack/src/main/java/eventos/security/SecurityConfig.java
@@ -14,6 +14,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -75,5 +79,19 @@ public class SecurityConfig {
                 return new BCryptPasswordEncoder().matches(rawPassword, encodedPassword);
             }
         };
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // Damos permiso explícito a tu Angular
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:9000/eventos/"));
+        // Permitimos todos los métodos
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        // Permitimos las cabeceras de seguridad
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
