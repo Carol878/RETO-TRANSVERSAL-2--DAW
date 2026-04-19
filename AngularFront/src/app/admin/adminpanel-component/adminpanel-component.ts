fd7d2533d431597de47013a142b53c4e439cf4a5
Nahir Carolina Torres <si.carolinatorres@gmail.com>
2026-04-19 22:48:55 +0200

    Implementar layout principal con menú lateral y área de contenido dinámico

--- AngularFront/src/app/admin/adminpanel-component/adminpanel-component.ts
+++ AngularFront/src/app/admin/adminpanel-component/adminpanel-component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-adminpanel-component',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive], // ¡Súper importante!
   templateUrl: './adminpanel-component.html',
-  styleUrl: './adminpanel-component.css',
+  styleUrl: './adminpanel-component.css'
 })
-export class AdminpanelComponent {}
+export class AdminpanelComponent {
+
+}
+/*import { Component } from '@angular/core';
+// 1. Importamos las herramientas de rutas
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-adminpanel-component',
+  standalone: true,
+  // 2. Las añadimos al array de imports
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './adminpanel-component.html',
+  styleUrl: './adminpanel-component.css'
+})
+export class AdminpanelComponent {
+
+}*/
