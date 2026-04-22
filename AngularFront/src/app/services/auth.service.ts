90c06b24a5613ccfa1d690d9732ba9d20ff27971
Antonio Jose Garcia <antoniojose.garcia179@comunidadunir.net>
2026-04-22 20:27:39 +0200

    añado entorno local y de producción para la url de la api, y actualizo los servicios para usar esta url dinámica.

--- AngularFront/src/app/services/auth.service.ts
+++ AngularFront/src/app/services/auth.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { jwtDecode} from 'jwt-decode';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
 
-private apiUrl = 'http://localhost:9000/';
+private apiUrl = `${environment.apiUrl}`;
 
   constructor(private http: HttpClient, private router: Router) {}
 
